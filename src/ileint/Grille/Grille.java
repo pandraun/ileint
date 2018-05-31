8051f46126b8c97c24540b69bc5bd2be943b9398
fodorg <fodorg@pc-dg-033-02.iut2.upmf-grenoble.fr>
2018-05-31 17:01:36 +0200

    debut squelette

--- src/ileint/Grille/Grille.java
+++ src/ileint/Grille/Grille.java
@@ -1,9 +1,12 @@
 package ileint.Grille;
 
+import ileint.Tuile.Tuile;
+import java.util.ArrayList;
+
 /**
  *
  * @author fodorg
  */
 public class Grille {
-    
+    private ArrayList<Tuile> tuiles;
 }
