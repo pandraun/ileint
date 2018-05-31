b2ea52e0ee5ba050f9003574af5a9c7d50755f03
fodorg <fodorg@pc-dg-033-02.iut2.upmf-grenoble.fr>
2018-05-31 17:41:27 +0200

    debut squelette

--- src/ileint/Joueur/Joueur.java
+++ src/ileint/Joueur/Joueur.java
@@ -11,10 +11,58 @@ import java.util.ArrayList;
  * @author fodorg
  */
 public class Joueur {
+
     private int numeroJoueur;
     private Aventurier role;
     private Tuile emplacementJoueur;
     private Controleur controleur;
     private ArrayList<CarteOrange> mainJoueur;
     
+    //Constructeur
+    public Joueur(int numeroJoueur, Controleur controleur){
+        
+    }
+
+    //Getters
+    public int getNumeroJoueur() {
+        return numeroJoueur;
+    }
+
+    public Aventurier getRole() {
+        return role;
+    }
+
+    public Tuile getEmplacementJoueur() {
+        return emplacementJoueur;
+    }
+
+    public Controleur getControleur() {
+        return controleur;
+    }
+
+    public ArrayList<CarteOrange> getMainJoueur() {
+        return mainJoueur;
+    }
+
+    //Setters
+    public void setNumeroJoueur(int numeroJoueur) {
+        this.numeroJoueur = numeroJoueur;
+    }
+
+    public void setRole(Aventurier role) {
+        this.role = role;
+    }
+
+    public void setEmplacementJoueur(Tuile emplacementJoueur) {
+        this.emplacementJoueur = emplacementJoueur;
+    }
+
+    public void setControleur(Controleur controleur) {
+        this.controleur = controleur;
+    }
+
+    public void setMainJoueur(ArrayList<CarteOrange> mainJoueur) {
+        this.mainJoueur = mainJoueur;
+    }
+
 }
