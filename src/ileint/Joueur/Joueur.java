e4506fe308849656448184a372d9e9ec9438cfcb
fodorg <fodorg@pc-dg-033-02.iut2.upmf-grenoble.fr>
2018-05-31 16:37:40 +0200

    debut squelette

---
+++ src/ileint/Joueur/Joueur.java
@@ -0,0 +1,20 @@
+package ileint.Joueur;
+
+import ileint.Aventurier.Aventurier;
+import ileint.Carte.CarteOrange;
+import ileint.Controleur.Controleur;
+import ileint.Tuile.Tuile;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fodorg
+ */
+public class Joueur {
+    private int numeroJoueur;
+    private Aventurier role;
+    private Tuile emplacementJoueur;
+    private Controleur controleur;
+    private ArrayList<CarteOrange> mainJoueur;
+    
+}
