07fc7f104699e5b45b690aea72b77f139d3eee16
fodorg <fodorg@pc-dg-033-02.iut2.upmf-grenoble.fr>
2018-05-31 16:37:21 +0200

    debut squelette

---
+++ src/ileint/Aventurier/Pion.java
@@ -0,0 +1,16 @@
+package ileint.Aventurier;
+
+
+
+/**
+ *
+ * @author fodorg
+ */
+public enum Pion {
+    ROUGE, //ingenieur
+    VERT, //explorateur
+    BLEU, //pilote
+    JAUNE, //navigateur
+    ORANGE, //messager ???
+    VIOLET; //plongeur
+}
