335f7e86f20774867d14ee21a7e7416ba1cc12a7
mazzolec <mazzolec@pc-dg-037-05.iut2.upmf-grenoble.fr>
2018-06-04 16:34:52 +0200

    J'suis en train de faire TUILE mdr et j'ai modif coordonnées,utils avec des commentaires lol
    grille etatTuile

--- src/ileint/Grille/Grille.java
+++ src/ileint/Grille/Grille.java
@@ -15,7 +15,7 @@ public class Grille {
 
     private HashMap<Coordonnee,Tuile> tuiles;
 
-    public Grille(ArrayList<Tuile> tuiles) {
+    public Grille(HashMap<Coordonnee,Tuile> tuiles) {
         this.tuiles = new HashMap<>();
 
     }
@@ -50,14 +50,47 @@ public class Grille {
         }
         return tuilesInondees;
     }
+    
+    public ArrayList<Tuile> getTuilesAccessibles() { // retourne toutes les cases accessibles du plateau (inondées + sèches)
+        ArrayList<Tuile> tuilesAccessibles = new ArrayList<>();
+        for (Tuile uneTuileAssechee : getTuilesAssechees()) {
+            tuilesAccessibles.add(uneTuileAssechee);
+        }
+        for (Tuile uneTuileInondee : getTuilesInondees()) {
+            tuilesAccessibles.add(uneTuileInondee);
+        }
+        return tuilesAccessibles;
+    }
 
-    public HashMap<Coordonnee,Tuile> getCasesCarreExclude(Tuile pos) { // retourne les 8 cases qui entourent la tuile (où le joueur se situe)
+    public HashMap<Coordonnee,Tuile> getCasesCarreExclude(Tuile pos) { // retourne les 8 cases qui entourent la tuile (où le joueur se situe) + null traité
         HashMap<Coordonnee,Tuile> casesCarreExclude = new HashMap<>();
         Coordonnee sauv;
             
         for (int i = -1; i<2; i++) {                                            // ligne
             for (int j = -1; j<2; i++) {                                        // colonne
                 if (i != 0 && j != 0) {                                         // ne prendra pas la case sur lequel l'aventurier se trouve
+                    sauv = new Coordonnee(pos.getCoordonnee().getX()+i,pos.getCoordonnee().getY()+j);
+                    if (getTuiles().get(sauv) != null ) {                       //si c'est pas un bord ou jsp
+                        casesCarreExclude.put(sauv,getTuiles().get(sauv));
+                    }
+                }
+                
+            }
+        }
+        return casesCarreExclude;
+    }
+    
+    public HashMap<Coordonnee,Tuile> getCasesCroixExclude(Tuile pos) { // retourne les 8 cases qui entourent la tuile (où le joueur se situe) + null traité
+        HashMap<Coordonnee,Tuile> casesCarreExclude = new HashMap<>();
+        Coordonnee sauv;
+            
+        for (int i = -1; i<2; i++) {                                            // ligne
+            for (int j = -1; j<2; i++) {                                        // colonne
+                if ((i != -1 && j != -1)
+                   ||(i != 1 && j != 1)
+                   ||(i != -1 && j != 1)
+                   ||(i != 1 && j != -1)
+                   ||(i != 0 && j != 0)) {                                       // ne prendra pas la case sur lequel l'aventurier se trouve
                     sauv = new Coordonnee(pos.getCoordonnee().getX()+i,pos.getCoordonnee().getY()+j);
                     if (getTuiles().get(sauv) != null ) {                       //si c'est pas un bord ou jsp
                         casesCarreExclude.put(sauv,getTuiles().get(sauv));
