d79b3b18c663b098abb385d78d441af89d9e1380
mazzolec <mazzolec@pc-dg-037-05.iut2.upmf-grenoble.fr>
2018-06-04 10:52:58 +0200

    J'suis en train de faire TUILE mdr et j'ai modif coordonnées,utils avec des commentaires lol
    grille etatTuile

--- src/ileint/Grille/Grille.java
+++ src/ileint/Grille/Grille.java
@@ -18,9 +18,9 @@ public class Grille {
         setLibelle(libelle);
     }
 //    
-    public boolean isCaseNull() {
+    
         
-    }
+    
     
     
     //-------------------------------- GETTERS SETTERS --------------------------------------------
