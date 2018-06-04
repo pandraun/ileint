c09706722de6e0b03c1c31a7fda5bba90f9dac59
mazzolec <mazzolec@pc-dg-037-05.iut2.upmf-grenoble.fr>
2018-06-04 17:00:51 +0200

    Méthodes de déplacement et assechement grille ajoutés lel des barres de rire

--- src/ileint/Grille/Grille.java
+++ src/ileint/Grille/Grille.java
@@ -13,9 +13,9 @@ import java.util.HashMap;
  */
 public class Grille {
 
-    private HashMap<Coordonnee,Tuile> tuiles;
+    private HashMap<Coordonnee, Tuile> tuiles;
 
-    public Grille(HashMap<Coordonnee,Tuile> tuiles) {
+    public Grille(HashMap<Coordonnee, Tuile> tuiles) {
         this.tuiles = new HashMap<>();
 
     }
@@ -50,74 +50,101 @@ public class Grille {
         }
         return tuilesInondees;
     }
-    
-    public ArrayList<Tuile> getTuilesAccessibles() { // retourne toutes les cases accessibles du plateau (inondées + sèches)
+
+    public ArrayList<Tuile> getTuilesAccessibles() { // retourne toutes les cases accessibles du plateau (inondées + sèches) + en principe y'a pas de null dedans
         ArrayList<Tuile> tuilesAccessibles = new ArrayList<>();
-        for (Tuile uneTuileAssechee : getTuilesAssechees()) {
-            tuilesAccessibles.add(uneTuileAssechee);
-        }
-        for (Tuile uneTuileInondee : getTuilesInondees()) {
-            tuilesAccessibles.add(uneTuileInondee);
-        }
+        tuilesAccessibles.addAll(getTuilesAssechees());
+        tuilesAccessibles.addAll(getTuilesInondees());
         return tuilesAccessibles;
     }
 
-    public HashMap<Coordonnee,Tuile> getCasesCarreExclude(Tuile pos) { // retourne les 8 cases qui entourent la tuile (où le joueur se situe) + null traité
-        HashMap<Coordonnee,Tuile> casesCarreExclude = new HashMap<>();
+    public HashMap<Coordonnee, Tuile> getCasesCarreExclude(Tuile pos) { // retourne les 8 cases qui entourent la tuile (où le joueur se situe) + null traité
+        HashMap<Coordonnee, Tuile> casesCarreExclude = new HashMap<>();
         Coordonnee sauv;
-            
-        for (int i = -1; i<2; i++) {                                            // ligne
-            for (int j = -1; j<2; i++) {                                        // colonne
+
+        for (int i = -1; i < 2; i++) {                                            // ligne
+            for (int j = -1; j < 2; i++) {                                        // colonne
                 if (i != 0 && j != 0) {                                         // ne prendra pas la case sur lequel l'aventurier se trouve
-                    sauv = new Coordonnee(pos.getCoordonnee().getX()+i,pos.getCoordonnee().getY()+j);
-                    if (getTuiles().get(sauv) != null ) {                       //si c'est pas un bord ou jsp
-                        casesCarreExclude.put(sauv,getTuiles().get(sauv));
+                    sauv = new Coordonnee(pos.getCoordonnee().getX() + i, pos.getCoordonnee().getY() + j);
+                    if (getTuiles().get(sauv) != null) {                       //si c'est pas un bord ou jsp
+                        casesCarreExclude.put(sauv, getTuiles().get(sauv));
                     }
                 }
-                
             }
         }
         return casesCarreExclude;
     }
-    
-    public HashMap<Coordonnee,Tuile> getCasesCroixExclude(Tuile pos) { // retourne les 8 cases qui entourent la tuile (où le joueur se situe) + null traité
-        HashMap<Coordonnee,Tuile> casesCarreExclude = new HashMap<>();
+
+    public HashMap<Coordonnee, Tuile> getCasesCroixExclude(Tuile pos) { // retourne les 4 cases qui entourent la tuile (où le joueur se situe) + null traité
+        HashMap<Coordonnee, Tuile> casesCroixExclude = new HashMap<>();
         Coordonnee sauv;
-            
-        for (int i = -1; i<2; i++) {                                            // ligne
-            for (int j = -1; j<2; i++) {                                        // colonne
+
+        for (int i = -1; i < 2; i++) {                                            // ligne
+            for (int j = -1; j < 2; i++) {                                        // colonne
                 if ((i != -1 && j != -1)
-                   ||(i != 1 && j != 1)
-                   ||(i != -1 && j != 1)
-                   ||(i != 1 && j != -1)
-                   ||(i != 0 && j != 0)) {                                       // ne prendra pas la case sur lequel l'aventurier se trouve
-                    sauv = new Coordonnee(pos.getCoordonnee().getX()+i,pos.getCoordonnee().getY()+j);
-                    if (getTuiles().get(sauv) != null ) {                       //si c'est pas un bord ou jsp
-                        casesCarreExclude.put(sauv,getTuiles().get(sauv));
+                        || (i != 1 && j != 1)
+                        || (i != -1 && j != 1)
+                        || (i != 1 && j != -1)
+                        || (i != 0 && j != 0)) {                                       // ne prendra pas la case sur lequel l'aventurier se trouve
+                    sauv = new Coordonnee(pos.getCoordonnee().getX() + i, pos.getCoordonnee().getY() + j);
+                    if (getTuiles().get(sauv) != null) {                       //si c'est pas un bord ou jsp
+                        casesCroixExclude.put(sauv, getTuiles().get(sauv));
                     }
                 }
-                
+
             }
         }
-        return casesCarreExclude;
+        return casesCroixExclude;
+    }
+
+    public HashMap<Coordonnee, Tuile> getCasesCarre(Tuile pos) { // retourne les 9 cases qui entourent la tuile (où le joueur se situe) incluse + null traité
+        HashMap<Coordonnee, Tuile> casesCarre = new HashMap<>();
+        Coordonnee sauv;
+
+        for (int i = -1; i < 2; i++) {                                            // ligne
+            for (int j = -1; j < 2; i++) {                                        // colonne
+                sauv = new Coordonnee(pos.getCoordonnee().getX() + i, pos.getCoordonnee().getY() + j);
+                if (getTuiles().get(sauv) != null) {                       //si c'est pas un bord ou jsp
+                    casesCarre.put(sauv, getTuiles().get(sauv));
+                }
+            }
+        }
+        return casesCarre;
+    }
+
+    public HashMap<Coordonnee, Tuile> getCasesCroix(Tuile pos) { // retourne les 4 cases qui entourent la tuile (où le joueur se situe) incluse + null traité
+        HashMap<Coordonnee, Tuile> casesCroix = new HashMap<>();
+        Coordonnee sauv;
+
+        for (int i = -1; i < 2; i++) {                                            // ligne
+            for (int j = -1; j < 2; i++) {                                        // colonne
+                if ((i != -1 && j != -1)
+                        || (i != 1 && j != 1)
+                        || (i != -1 && j != 1)
+                        || (i != 1 && j != -1)) {
+                    sauv = new Coordonnee(pos.getCoordonnee().getX() + i, pos.getCoordonnee().getY() + j);
+                    if (getTuiles().get(sauv) != null) {                       //si c'est pas un bord ou jsp
+                        casesCroix.put(sauv, getTuiles().get(sauv));
+                    }
+                }
+            }
+        }
+        return casesCroix;
     }
 
     //-------------------------------- GETTERS SETTERS --------------------------------------------
-
     /**
      * @return the tuiles
      */
-    public HashMap<Coordonnee,Tuile> getTuiles() {
+    public HashMap<Coordonnee, Tuile> getTuiles() {
         return tuiles;
     }
 
     /**
      * @param tuiles the tuiles to set
      */
-    public void setTuiles(HashMap<Coordonnee,Tuile> tuiles) {
+    public void setTuiles(HashMap<Coordonnee, Tuile> tuiles) {
         this.tuiles = tuiles;
     }
 
-
-    
 }
