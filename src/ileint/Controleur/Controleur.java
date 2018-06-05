4ccb0830bad8013d890fd0c6477e32dd57ab458e
piolleta <piolleta@pc-dg-039-11.iut2.upmf-grenoble.fr>
2018-06-05 15:47:00 +0200

    Fini initialisation tuile jeu: 5 juin 15h46

--- src/ileint/Controleur/Controleur.java
+++ src/ileint/Controleur/Controleur.java
@@ -11,6 +11,7 @@ import ileint.Tuile.Tuile;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import util.TypeTresor;
 import util.Utils;
 
 /**
@@ -41,42 +42,44 @@ public class Controleur {
         Collections.shuffle(piocheOrange);
 
         //Initialisation des tuiles à la main
-        Tuile t00 = new Tuile(null, new Coordonnee(0, 0), null, null);
+        Tuile t00 = new Tuile(null, new Coordonnee(0, 0), null);
         Tuile t01 = new Tuile(null, new Coordonnee(0, 1), null, null);
-        Tuile t02 = new Tuile(NomTuile.Le_Pont_des_Abimes, new Coordonnee(0, 2), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t03 = new Tuile(NomTuile.La_Porte_de_Bronze, new Coordonnee(0, 3), Utils.EtatTuile.INONDEE, null);
-        Tuile t04 = new Tuile(null, new Coordonnee(0, 4), null, null);
-        Tuile t05 = new Tuile(null, new Coordonnee(0, 5), null, null);
-        Tuile t10 = new Tuile(null, new Coordonnee(1, 0), null, null);
-        Tuile t11 = new Tuile(NomTuile.La_Caverne_des_Ombres, new Coordonnee(1, 1), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t12 = new Tuile(NomTuile.La_Porte_de_Fer, new Coordonnee(1, 2), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t13 = new Tuile(NomTuile.La_Porte_d_Or, new Coordonnee(1, 3), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t14 = new Tuile(NomTuile.Les_Falaises_de_l_Oubli, new Coordonnee(1, 4),  Utils.EtatTuile.ASSECHEE, null);
-        Tuile t15 = new Tuile(null, new Coordonnee(1, 5), null, null);
-        Tuile t20 = new Tuile(NomTuile.Le_Palais_de_Corail, new Coordonnee(2, 0), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t21 = new Tuile(NomTuile.La_Porte_d_Argent, new Coordonnee(2, 1), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t22 = new Tuile(NomTuile.Les_Dunes_de_l_Illusion, new Coordonnee(2, 2), Utils.EtatTuile.COULEE, null);
-        Tuile t23 = new Tuile(NomTuile.Heliport, new Coordonnee(2, 3), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t24 = new Tuile(NomTuile.La_Porte_de_Cuivre, new Coordonnee(2, 4), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t25 = new Tuile(NomTuile.Le_Jardin_des_Hurlements, new Coordonnee(2, 5), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t30 = new Tuile(NomTuile.La_Foret_Pourpre, new Coordonnee(3, 0), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t31 = new Tuile(NomTuile.Le_Lagon_Perdu, new Coordonnee(3, 1), Utils.EtatTuile.INONDEE, null);
-        Tuile t32 = new Tuile(NomTuile.Le_Marais_Brumeux, new Coordonnee(3, 2), Utils.EtatTuile.COULEE, null);
-        Tuile t33 = new Tuile(NomTuile.Observatoire, new Coordonnee(3, 3), Utils.EtatTuile.INONDEE, null);
-        Tuile t34 = new Tuile(NomTuile.Le_Rocher_Fantome, new Coordonnee(3, 4),Utils.EtatTuile.COULEE, null);
-        Tuile t35 = new Tuile(NomTuile.La_Caverne_du_Brasier, new Coordonnee(3, 5), Utils.EtatTuile.INONDEE, null);
-        Tuile t40 = new Tuile(null, new Coordonnee(4, 0), null, null);
-        Tuile t41 = new Tuile(NomTuile.Le_Temple_du_Soleil, new Coordonnee(4, 1), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t42 = new Tuile(NomTuile.Le_Temple_de_La_Lune, new Coordonnee(4, 2), Utils.EtatTuile.COULEE, null);
-        Tuile t43 = new Tuile(NomTuile.Le_Palais_des_Marees, new Coordonnee(4, 3), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t44 = new Tuile(NomTuile.Le_Val_du_Crepuscule, new Coordonnee(4, 4), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t45 = new Tuile(null, new Coordonnee(4, 5), null, null);
-        Tuile t50 = new Tuile(null, new Coordonnee(5, 0), null, null);
-        Tuile t51 = new Tuile(null, new Coordonnee(5, 1), null, null);
-        Tuile t52 = new Tuile(NomTuile.La_Tour_du_Guet, new Coordonnee(5, 2), Utils.EtatTuile.ASSECHEE, null);
-        Tuile t53 = new Tuile(NomTuile.Le_Jardin_des_Murmures, new Coordonnee(5, 3), Utils.EtatTuile.INONDEE, null);
-        Tuile t54 = new Tuile(null, new Coordonnee(5, 4), null, null);
-        Tuile t55 = new Tuile(null, new Coordonnee(5, 5), null, null);
+        Tuile t02 = new Tuile(NomTuile.Le_Pont_des_Abimes, new Coordonnee(0, 2), Utils.EtatTuile.ASSECHEE);
+        Tuile t03 = new Tuile(NomTuile.La_Porte_de_Bronze, new Coordonnee(0, 3), Utils.EtatTuile.INONDEE);
+        Tuile t04 = new Tuile(null, new Coordonnee(0, 4), null);
+        Tuile t05 = new Tuile(null, new Coordonnee(0, 5), null);
+        Tuile t10 = new Tuile(null, new Coordonnee(1, 0), null);
+        Tuile t11 = new Tuile(NomTuile.La_Caverne_des_Ombres, new Coordonnee(1, 1), Utils.EtatTuile.ASSECHEE,TypeTresor.CRISTAL);
+        Tuile t12 = new Tuile(NomTuile.La_Porte_de_Fer, new Coordonnee(1, 2), Utils.EtatTuile.ASSECHEE);
+        Tuile t13 = new Tuile(NomTuile.La_Porte_d_Or, new Coordonnee(1, 3), Utils.EtatTuile.ASSECHEE);
+        Tuile t14 = new Tuile(NomTuile.Les_Falaises_de_l_Oubli, new Coordonnee(1, 4),  Utils.EtatTuile.ASSECHEE);
+        Tuile t15 = new Tuile(null, new Coordonnee(1, 5), null);
+        Tuile t20 = new Tuile(NomTuile.Le_Palais_de_Corail, new Coordonnee(2, 0), Utils.EtatTuile.ASSECHEE,TypeTresor.CALICE);
+        Tuile t21 = new Tuile(NomTuile.La_Porte_d_Argent, new Coordonnee(2, 1), Utils.EtatTuile.ASSECHEE);
+        Tuile t22 = new Tuile(NomTuile.Les_Dunes_de_l_Illusion, new Coordonnee(2, 2), Utils.EtatTuile.COULEE);
+        Tuile t23 = new Tuile(NomTuile.Heliport, new Coordonnee(2, 3), Utils.EtatTuile.ASSECHEE);
+        Tuile t24 = new Tuile(NomTuile.La_Porte_de_Cuivre, new Coordonnee(2, 4), Utils.EtatTuile.ASSECHEE);
+        Tuile t25 = new Tuile(NomTuile.Le_Jardin_des_Hurlements, new Coordonnee(2, 5), Utils.EtatTuile.ASSECHEE,TypeTresor.STATUE);
+        Tuile t30 = new Tuile(NomTuile.La_Foret_Pourpre, new Coordonnee(3, 0), Utils.EtatTuile.ASSECHEE);
+        Tuile t31 = new Tuile(NomTuile.Le_Lagon_Perdu, new Coordonnee(3, 1), Utils.EtatTuile.INONDEE);
+        Tuile t32 = new Tuile(NomTuile.Le_Marais_Brumeux, new Coordonnee(3, 2), Utils.EtatTuile.COULEE);
+        Tuile t33 = new Tuile(NomTuile.Observatoire, new Coordonnee(3, 3), Utils.EtatTuile.INONDEE);
+        Tuile t34 = new Tuile(NomTuile.Le_Rocher_Fantome, new Coordonnee(3, 4),Utils.EtatTuile.COULEE);
+        Tuile t35 = new Tuile(NomTuile.La_Caverne_du_Brasier, new Coordonnee(3, 5), Utils.EtatTuile.INONDEE,TypeTresor.CRISTAL);
+        Tuile t40 = new Tuile(null, new Coordonnee(4, 0), null);
+        Tuile t41 = new Tuile(NomTuile.Le_Temple_du_Soleil, new Coordonnee(4, 1), Utils.EtatTuile.ASSECHEE,TypeTresor.PIERRE);
+        Tuile t42 = new Tuile(NomTuile.Le_Temple_de_La_Lune, new Coordonnee(4, 2), Utils.EtatTuile.COULEE,TypeTresor.PIERRE);
+        Tuile t43 = new Tuile(NomTuile.Le_Palais_des_Marees, new Coordonnee(4, 3), Utils.EtatTuile.ASSECHEE,TypeTresor.CALICE);
+        Tuile t44 = new Tuile(NomTuile.Le_Val_du_Crepuscule, new Coordonnee(4, 4), Utils.EtatTuile.ASSECHEE);
+        Tuile t45 = new Tuile(null, new Coordonnee(4, 5), null);
+        Tuile t50 = new Tuile(null, new Coordonnee(5, 0), null);
+        Tuile t51 = new Tuile(null, new Coordonnee(5, 1), null);
+        Tuile t52 = new Tuile(NomTuile.La_Tour_du_Guet, new Coordonnee(5, 2), Utils.EtatTuile.ASSECHEE);
+        Tuile t53 = new Tuile(NomTuile.Le_Jardin_des_Murmures, new Coordonnee(5, 3), Utils.EtatTuile.INONDEE,TypeTresor.STATUE);
+        Tuile t54 = new Tuile(null, new Coordonnee(5, 4), null);
+        Tuile t55 = new Tuile(null, new Coordonnee(5, 5), null);
+        
+       
 
     }
 
