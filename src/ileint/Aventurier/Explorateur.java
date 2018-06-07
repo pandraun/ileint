7c9fb6b07f7ee2ae17d328a023f441bf943cd8e5
fodorg <fodorg@pc-dg-033-02.iut2.upmf-grenoble.fr>
2018-06-07 15:46:30 +0200

    Position Vue Aventurier

--- src/ileint/Aventurier/Explorateur.java
+++ src/ileint/Aventurier/Explorateur.java
@@ -42,7 +42,7 @@ public class Explorateur extends Aventurier {
         boolean saisieCorrecte;
         HashMap<Coordonnee,Tuile> casesBruts = new HashMap<>();
         HashMap<Coordonnee,Tuile> casesTraitées = new HashMap<>();
-
+        
         System.out.println("==== Instructions pour amorcer un déplacement ====");
         System.out.println("Cases où le déplacement est possible :");
 
@@ -50,26 +50,26 @@ public class Explorateur extends Aventurier {
         casesTraitées = joueur.getControleur().getGrille().getTuilesDeplacementPossible(casesBruts);
         
         for (Tuile uneTuile : casesTraitées.values()) { //affiche les cases accessibles
-            System.out.println(uneTuile.getNom());
+            System.out.println("/"+uneTuile.getNom());
         }
 
         System.out.println("Saisir le nom (exact) de la case où se déplacer");
         String entree = sc.nextLine();
 
         for (Tuile uneTuile : casesTraitées.values()) { //cherche la correspondance entre l'entrée scanner et la tuile
-            if (entree == uneTuile.getNom().toString()) {
+            if (entree.equals(uneTuile.getNom().toString())) {
                 sauv = uneTuile; // le code pense qu'il n'a pas tjrs une valeur mais il en aura tjrs une;
             }
         }
 
         saisieCorrecte = false;
-        while (saisieCorrecte == false) {
+        while (!saisieCorrecte) {
             if (casesTraitées.containsValue(sauv)) {
-                tuileDepart.getJoueursTuile().remove(joueur);       // retire le joueur dans la liste des joueurs de la tuile d'arrivée
+                joueur.getEmplacementJoueur().getJoueursTuile().remove(joueur);       // retire le joueur dans la liste des joueurs de la tuile d'arrivée
                 sauv.getJoueursTuile().add(joueur);     // ajoute le joueur dans la liste des joueurs de la tuile d'arrivée
                 joueur.setEmplacementJoueur(sauv);   //affecte l'emplacement du joueur à la nouvelle tuile où il se trouve
                 saisieCorrecte = true;
-                System.out.println("Déplacement du " + joueur + " effectué " + tuileDepart.getNom() + " vers " + sauv.getNom() + ".");
+                System.out.println("Déplacement du " + getNom() + " effectué " + tuileDepart.getNom() + " vers " + sauv.getNom() + ".");
             } else {
                 System.out.println("Saisie incorrecte, veuillez recommencer la saisie");
                 entree = sc.nextLine();
